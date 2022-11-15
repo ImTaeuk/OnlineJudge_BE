419ba0cd524ae6f9b10556b9ff96b073bf48863f
ImTaeuk <ktu3862@gmail.com>
2022-11-15 14:45:39 +0900

    Submission requests to GDB

--- answer/views/oj.py
+++ answer/views/oj.py
@@ -2,7 +2,6 @@ from utils.api import APIView, validate_serializer
 from account.decorators import login_required
 from account.models import User
 from ..models import Answer
-from ..serializers import CreateAnswerSerializer
 from question.models import Question
 
 class AnswerAPI(APIView):
@@ -11,14 +10,22 @@ class AnswerAPI(APIView):
     def post(self, request):
         data = request.data
 
+        question = Question.objects.get(id=data["question_id"])
+        
+        if (question.answer_registered):
+                answer = Answer.objects.get(question_id=data["question_id"])
+                answer.content = data["content"]
+                answer.save()
+                return self.success(answer)
+            
+
         _created_by = User.objects.get(user_name=request.username)
 
-        answer = answer.objects.create(
+        answer = Answer.objects.create(
             created_by = _created_by,
             submission_id = data["submission_id"],
             content = data["content"],
             question_id = data["question_id"],
-
         )
         answer.save()
 
@@ -27,11 +34,11 @@ class AnswerAPI(APIView):
         question.answer_id = answer.id
         question.save()
 
-        return self.success({"answer_id" : answer.id})
+        return self.success(answer)
 
     def get(self, request):
         try:
-             answer = Answer.objects.get(id=request.GET.get("id"))
+             answer = Answer.objects.get(question_id=request.GET.get("question_id"))
         except Answer.DoesNotExist:
             return self.error("Invalid Answer ID")
         return self.success(answer)
--- submission/urls/admin.py
+++ submission/urls/admin.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ..views.admin import SubmissionRejudgeAPI
+from ..views.admin import SubmissionRejudgeAPI, SubmissionVisualDataResultAPI
 
 urlpatterns = [
     url(r"^submission/rejudge?$", SubmissionRejudgeAPI.as_view(), name="submission_rejudge_api"),
+    url(r"^submission/submissionVisualData?$", SubmissionVisualDataResultAPI.as_view(), name="submission_visual_data"),
 ]
